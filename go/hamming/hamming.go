2cce94785473f8a7e325d99cc54f9c0f0a55ab8f
Dan Rusei <dan.rusei@gmail.com>
2020-01-09 10:24:45 +0200

    Go hamming submited

---
+++ go/hamming/hamming.go
@@ -0,0 +1,20 @@
+package hamming
+
+import "fmt"
+
+//Distance caculate the Hamming Distance between two DNA strands
+func Distance(a, b string) (int, error) {
+
+	if len(a) != len(b) {
+		return 0, fmt.Errorf("strings are not equal")
+	}
+
+	var distance int
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			distance++
+		}
+	}
+
+	return distance, nil
+}
