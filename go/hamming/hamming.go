1b984047047ec48ea8d0cecbc872338b2c4ebc6d
Dan Rusei <dan.rusei@gmail.com>
2020-01-11 17:55:02 +0200

    GO hamming using RuneCountInString

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -8,15 +8,15 @@ import (
 //Distance caculate the Hamming Distance between two DNA strands
 func Distance(a, b string) (int, error) {
 
-	if len([]rune(a)) != len([]rune(b)) {
+	if utf8.RuneCountInString(a) != utf8.RuneCountInString(b) {
 		return 0, errors.New("strings are not equal")
 	}
 
 	var distance int
 
 	for len(a) > 0 {
-		rA, sizeA := utf8.DecodeRune([]byte(a))
-		rB, sizeB := utf8.DecodeRune([]byte(b))
+		rA, sizeA := utf8.DecodeRuneInString(a)
+		rB, sizeB := utf8.DecodeRuneInString(b)
 
 		if rA != rB {
 			distance++
