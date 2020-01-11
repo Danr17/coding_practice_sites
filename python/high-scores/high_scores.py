edccb4b776727722a348d874bb3013ef4b33d58e
Dan Rusei <dan.rusei@gmail.com>
2020-01-11 19:04:04 +0200

    Python high_scores simplified, sort vs sorted

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -5,12 +5,11 @@ def latest(scores):
 def personal_best(scores):
     return max(scores)
 
-
 def personal_top_three(scores):
-    top_three = []
-    for _ in range(min(3, len(scores))):
-        top_three.append(max(scores))
-        scores.remove(max(scores))
-    return top_three
+    index = min(3, len(scores))
+    scores.sort(reverse=True)
+    return scores[:index]
+
+
 
 
