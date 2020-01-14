a12149f66b73ba928ae820bda44ed8a5780760ae
Dan Rusei <dan.rusei@gmail.com>
2020-01-14 12:55:25 +0200

    raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,26 +1,23 @@
 package raindrops
 
-func Convert(input int) string {
-}
+import "strconv"
 
-/*
-//Convert check modulo and add Ping, Plnag , Plong if the case
-func Convert(inputint) string {
+//Convert check modulo and add Pling, Plnag , Plong if the case
+func Convert(input int) string {
 	var output string
 	if input%3 == 0 {
-		utput += "Pling"
+		output += "Pling"
 	}
 	if input%5 == 0 {
-		utput += "Plang"
+		output += "Plang"
 	}
 	if input%7 == 0 {
-		utput += "Plong"
-}
+		output += "Plong"
+	}
 
 	if output == "" {
-		eturn strconv.Itoa(input)
+		return strconv.Itoa(input)
 	}
-return output
+	return output
 
 }
-*/
