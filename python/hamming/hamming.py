ee7a99dd1208491778b5d019dd9d9a99a830eee8
Dan Rusei <dan.rusei@gmail.com>
2020-02-13 15:11:56 +0200

    python resubmiting hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -3,8 +3,8 @@ def distance(strand_a, strand_b):
         raise ValueError("The strands have different lengths, should be equal.")
     
     count = 0
-    for i, _ in enumerate(strand_a):
-        if strand_a[i] != strand_b[i]:
+    for a, b in zip(strand_a, strand_b):
+        if a != b:
             count +=1
     
     return count
