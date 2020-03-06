3e70b85de407a940fc1a07597ef7baaabd4d7400
Dan Rusei <dan.rusei@gmail.com>
2020-03-06 18:39:58 +0200

    changes to isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,9 +1,7 @@
 def is_isogram(string: str) -> bool:
     duplicates = set()
-    for _, s in enumerate(string):
-        s_lower = s.lower()
-        if s.isalpha() and s_lower in duplicates:
+    for _, s in enumerate(string.lower()):
+        if s.isalpha() and s in duplicates:
             return False
-        duplicates.add(s_lower)
+        duplicates.add(s)
     return True
-
