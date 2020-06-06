bb3b212efb3b9221c50304a4ef7bd08c90fae0d0
Dan Rusei <dan.rusei@gmail.com>
2020-06-06 17:31:03 +0300

    python twelve_days

--- Exercism_scripts/python/twelve-days/twelve_days.py
+++ Exercism_scripts/python/twelve-days/twelve_days.py
@@ -1,25 +1,36 @@
-
-blob = "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.\
-On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.\
-On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\
-On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\
-On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\
-On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\
-On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\
-On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\
-On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\
-On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\
-On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\
-On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-
 def recite(start_verse, end_verse):
-    d = "."
-    song = [e+d for e in blob.split(d) if e]
-    if start_verse > end_verse or end_verse > len(song) + 1:
+    numbers = {1:"first", 2:"second", 3:"third", 4:"fourth", 5: "fifth", 6:"sixth", 7:"seventh", 8:"eighth", 9:"ninth", 10:"tenth", 11:"eleventh", 12:"twelfth"}
+    what = {1: "a Partridge in a Pear Tree",
+    2: "two Turtle Doves",
+    3: "three French Hens",
+    4: "four Calling Birds",
+    5: "five Gold Rings",
+    6: "six Geese-a-Laying",
+    7: "seven Swans-a-Swimming",
+    8: "eight Maids-a-Milking",
+    9: "nine Ladies Dancing",
+    10: "ten Lords-a-Leaping",
+    11: "eleven Pipers Piping",
+    12: "twelve Drummers Drumming"
+    }
+
+    if start_verse > end_verse or end_verse > 12:
         raise Exception("start_verse should be smaller than end_verse and end_verse should be smaller or equal to length of the song")
     lyrics = []  
     while start_verse <= end_verse:
-        lyrics.append(song[start_verse -1])
+        line = f'On the {numbers[start_verse]} day of Christmas my true love gave to me: '
+        i = start_verse
+        if i == 1 :
+            line += what[i] + "."
+        else:
+            while i > 0:
+                if i == 1:
+                    line += "and " + what[i] + "."
+                    i -= 1
+                else:
+                    line +=  what[i] + ", "
+                    i -= 1
+        lyrics.append(line)
         start_verse += 1
     return lyrics
 
