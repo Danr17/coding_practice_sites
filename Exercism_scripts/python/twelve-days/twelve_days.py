5e4d428ba31788f2b80a709a2c9c0f7a5bba6d32
Dan Rusei <dan.rusei@gmail.com>
2020-06-09 10:01:25 +0300

    python twelve_days

--- Exercism_scripts/python/twelve-days/twelve_days.py
+++ Exercism_scripts/python/twelve-days/twelve_days.py
@@ -1,37 +1,42 @@
-def recite(start_verse, end_verse):
-    numbers = {1:"first", 2:"second", 3:"third", 4:"fourth", 5: "fifth", 6:"sixth", 7:"seventh", 8:"eighth", 9:"ninth", 10:"tenth", 11:"eleventh", 12:"twelfth"}
-    what = {1: "a Partridge in a Pear Tree",
-    2: "two Turtle Doves",
-    3: "three French Hens",
-    4: "four Calling Birds",
-    5: "five Gold Rings",
-    6: "six Geese-a-Laying",
-    7: "seven Swans-a-Swimming",
-    8: "eight Maids-a-Milking",
-    9: "nine Ladies Dancing",
-    10: "ten Lords-a-Leaping",
-    11: "eleven Pipers Piping",
-    12: "twelve Drummers Drumming"
-    }
 
+numbers = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"]
+what = ["a Partridge in a Pear Tree",
+    "two Turtle Doves",
+    "three French Hens",
+    "four Calling Birds",
+    "five Gold Rings",
+    "six Geese-a-Laying",
+    "seven Swans-a-Swimming",
+    "eight Maids-a-Milking",
+    "nine Ladies Dancing",
+    "ten Lords-a-Leaping",
+    "eleven Pipers Piping",
+    "twelve Drummers Drumming"
+]
+
+def recite(start_verse: int, end_verse: int) -> list:
     if start_verse > end_verse or end_verse > 12:
         raise Exception("start_verse should be smaller than end_verse and end_verse should be smaller or equal to length of the song")
-    lyrics = []  
-    while start_verse <= end_verse:
-        line = f'On the {numbers[start_verse]} day of Christmas my true love gave to me: '
-        i = start_verse
-        if i == 1 :
-            line += what[i] + "."
+    
+    lyrics = []
+    
+    for i in range(start_verse, end_verse + 1):
+        line = f'On the {numbers[i - 1]} day of Christmas my true love gave to me: '
+
+        if i == 1:
+            line += what[i -1] + "."
         else:
-            while i > 0:
-                if i == 1:
-                    line += "and " + what[i] + "."
-                    i -= 1
-                else:
-                    line +=  what[i] + ", "
-                    i -= 1
-        lyrics.append(line)
-        start_verse += 1
+            s = i
+            while s > 0: 
+                line += get_lyric(s - 1)
+                s -= 1
+    
+        lyrics.append(line) 
+        
     return lyrics
 
-
+def get_lyric(line: int) -> str:
+    if line == 0:
+        return "and " + what[line] + "."
+    else:
+        return  what[line] + ", "
