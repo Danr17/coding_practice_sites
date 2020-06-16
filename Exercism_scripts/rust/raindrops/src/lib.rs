969d62fa348eacc5f8a35bff09d264b1ba92fbc9
Dan Rusei <dan.rusei@gmail.com>
2020-06-16 16:31:03 +0300

    rust raindrops

---
+++ Exercism_scripts/rust/raindrops/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn raindrops(n: u32) -> String {
+    let mut out: String = String::new();
+    if n % 3 == 0 {
+        out += "Pling";
+    }
+    if n % 5 == 0 {
+        out += "Plang";
+    }
+    if n % 7 == 0 {
+        out += "Plong";
+    }
+    if out.is_empty() {
+        return n.to_string();
+    }
+    out
+}
