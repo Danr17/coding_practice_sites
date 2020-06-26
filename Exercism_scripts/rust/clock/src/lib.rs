e88927ccc12e925e5e32b528f8d192ed5fa7db67
Dan Rusei <dan.rusei@gmail.com>
2020-06-26 16:29:58 +0300

    rust updated clock

--- Exercism_scripts/rust/clock/src/lib.rs
+++ Exercism_scripts/rust/clock/src/lib.rs
@@ -5,9 +5,14 @@ pub struct Clock {
     minutes: i32,
 }
 
+const MINUTES_PER_DAY: i32 = 24 * 60;
+const MINUTES_PER_HOUR: i32 = 60;
+
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let min = (((hours * 60 + minutes) % (24 * 60)) + 24 * 60) % (24 * 60);
+        let min = (((hours * MINUTES_PER_HOUR + minutes).rem_euclid(MINUTES_PER_DAY))
+            + MINUTES_PER_DAY)
+            .rem_euclid(MINUTES_PER_DAY);
         Clock { minutes: min }
     }
     pub fn add_minutes(&self, minutes: i32) -> Self {
@@ -16,8 +21,8 @@ impl Clock {
 }
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let hour = self.minutes / 60;
-        let minute = self.minutes % 60;
+        let hour = self.minutes.div_euclid(MINUTES_PER_HOUR);
+        let minute = self.minutes.rem_euclid(MINUTES_PER_HOUR);
         write!(f, "{:#02}:{:#02}", hour, minute)
     }
 }
