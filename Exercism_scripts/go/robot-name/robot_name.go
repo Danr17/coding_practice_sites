85835bbad70c03181f675891d397589c03de7102
Dan Rusei <dan.rusei@gmail.com>
2020-07-06 18:27:04 +0300

    cleaning up the functions

--- Exercism_scripts/go/robot-name/robot_name.go
+++ Exercism_scripts/go/robot-name/robot_name.go
@@ -17,19 +17,23 @@ func (r *Robot) Name() (string, error) {
 	if *r != "" {
 		return r.String(), nil
 	}
+	r.generate()
+
+	return r.String(), nil
+}
+
+// Reset the robot name
+func (r *Robot) Reset() {
+	r.generate()
+}
+
+func (r *Robot) generate() {
 	rand.Seed(time.Now().UnixNano())
 	rand.Shuffle(len(arr), func(i, j int) {
 		arr[i], arr[j] = arr[j], arr[i]
 	})
 	name := arr[0] + arr[1] + strconv.Itoa(rand.Intn(999-100)+100)
 	*r = Robot(name)
-	return r.String(), nil
-}
-
-// Reset the robot name
-func (r *Robot) Reset() {
-	*r = ""
-	_, _ = r.Name()
 }
 
 // Reset the robot name
