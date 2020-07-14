c6fb447c679185f80fdeaf58d541f2bf51c2e1b1
Dan Rusei <dan.rusei@gmail.com>
2020-07-14 12:15:20 +0300

    go robot name submited

--- Exercism_scripts/go/robot-name/robot_name.go
+++ Exercism_scripts/go/robot-name/robot_name.go
@@ -6,33 +6,35 @@ import (
 	"time"
 )
 
-var arr = [...]string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-	"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
-
 //Robot holds the name
 type Robot string
+
+var exist = map[string]bool{}
 
 //Name generate robot's name
 func (r *Robot) Name() (string, error) {
 	if *r != "" {
 		return r.String(), nil
 	}
-	r.generate()
+	for {
+
+		r.Reset()
+		if _, ok := exist[r.String()]; ok {
+			continue
+		}
+		exist[r.String()] = true
+		break
+	}
 
 	return r.String(), nil
 }
 
 // Reset the robot name
 func (r *Robot) Reset() {
-	r.generate()
-}
 
-func (r *Robot) generate() {
 	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(arr), func(i, j int) {
-		arr[i], arr[j] = arr[j], arr[i]
-	})
-	name := arr[0] + arr[1] + strconv.Itoa(rand.Intn(999-100)+100)
+	name := string('A'+rand.Intn(26)) + string('A'+rand.Intn(26)) + strconv.Itoa(rand.Intn(999-100)+100)
+
 	*r = Robot(name)
 }
 
