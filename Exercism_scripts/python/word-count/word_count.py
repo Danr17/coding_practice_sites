437fc52224a0e126e29b39f00702a55de5c0df8e
Dan Rusei <dan.rusei@gmail.com>
2020-08-21 10:03:08 +0300

    python world-count refactoring

--- Exercism_scripts/python/word-count/word_count.py
+++ Exercism_scripts/python/word-count/word_count.py
@@ -1,28 +1,8 @@
-import string
+import re
+from collections import Counter
 
 def count_words(sentence):
-    words = {}
-    last_index = 0
-    valid_letters = string.ascii_letters + string.digits + "'"
-
-    for idx, char in enumerate(sentence):
-        if char in valid_letters:
-            if idx != len(sentence) -1:
-                continue
-            else:
-                idx +=1
-        word = sentence[last_index:idx].lower()
-        
-        while word.startswith("'"):
-            word =  word[1:-1]      
-        
-        if word not in string.whitespace:
-            if word not in words.keys():
-                words[word] = 1
-            else:
-                words[word] += 1
-        last_index = idx + 1
-    
-    return words
+    sentence = re.sub(r'[^a-z0-9 \']', ' ', sentence.lower())
+    return Counter([word.strip("'") for word in sentence.split()])
 
 
