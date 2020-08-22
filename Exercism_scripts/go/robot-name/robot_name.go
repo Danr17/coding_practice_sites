51e9b845e1eed2345bfa6e566f90a81ac112db20
Dan Rusei <dan.rusei@gmail.com>
2020-08-22 14:34:44 +0300

    go robot use generated ints for maps

--- Exercism_scripts/go/robot-name/robot_name.go
+++ Exercism_scripts/go/robot-name/robot_name.go
@@ -1,9 +1,9 @@
 package robotname
 
 import (
-	"fmt"
 	"log"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
@@ -12,11 +12,15 @@ const maxGenNames int = 26 * 26 * 10 * 10 * 10
 //Robot holds the name
 type Robot string
 
-var exist = map[string]bool{}
+var exist = map[int]bool{}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 //Name generate robot's name
 func (r *Robot) Name() (string, error) {
-	if *r != "" {
+	if len(*r) != 0 {
 		return r.String(), nil
 	}
 	r.Reset()
@@ -26,24 +30,32 @@ func (r *Robot) Name() (string, error) {
 // Reset the robot name
 func (r *Robot) Reset() {
 	for {
-		if len(exist) == maxGenNames-1 {
+		if len(exist) == maxGenNames {
 			log.Fatalln("you reached the maximum numer of names")
 		}
 
-		r.generateName()
-		if exist[r.String()] {
+		num := generateName()
+		if exist[num] {
 			continue
 		}
-		exist[r.String()] = true
+		exist[num] = true
+
+		stringify := strconv.Itoa(num)
+		lit1, _ := strconv.Atoi(stringify[0:2])
+		lit2, _ := strconv.Atoi(stringify[2:4])
+		name := string(lit1) + string(lit2) + stringify[4:]
+		*r = Robot(name)
 		break
 	}
 }
 
-func (r *Robot) generateName() {
-	rand.Seed(time.Now().UnixNano())
-	number := fmt.Sprintf("%03d", rand.Intn(999))
-	name := string('A'+rand.Intn(26)) + string('A'+rand.Intn(26)) + number
-	*r = Robot(name)
+func generateName() int {
+	num := rand.Intn(999)
+	return ('A'+rand.Intn(26))*100000 + ('A'+rand.Intn(26))*1000 + num
+
+	//number := fmt.Sprintf("%03d", rand.Intn(999))
+	//name := string('A'+rand.Intn(26)) + string('A'+rand.Intn(26)) + number
+	//copy(buf, name)
 }
 
 // string method
